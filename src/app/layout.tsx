461e5e12e07f02ac832ff9de29602243e0fc6300
Sandrine Gamman <sandrw14@uia.no>
2024-10-03 20:28:38 +0200

    ads on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,8 @@ export default function Layout({ children }: { children: ReactNode }) {
           <NavBar />
           <script 
             async 
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9490987391720050"crossorigin="anonymous">
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9490987391720050"
+            crossOrigin="anonymous">
 
           </script>
         </header>
