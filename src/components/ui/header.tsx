b15996fb914bbc5631f39d46c0f24c13f98c38c0
Sandrine Gamman <sandrw14@uia.no>
2024-10-03 11:23:27 +0200

    Refactor image sources in header component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -23,6 +23,8 @@ export default function Header() {
           Contact Us
         </button>
       </Link>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9490987391720050"
+     crossOrigin="anonymous"></script>
     </header>
   );
 }
