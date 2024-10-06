aacb780b3e59a21d1d97a6fb788cd655234d8fd9
Sandrine Gamman <sandrw14@uia.no>
2024-10-06 19:58:22 +0200

    Refactor layout component and add favicon and meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,17 @@ export default function Layout({ children }: { children: ReactNode }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9490987391720050"
           crossOrigin="anonymous"
         />
+
+      
+        <link rel="icon" href="/braiderylogo-removebg-preview.png" />
+        {/* You can also add multiple sizes for different devices */}
+        {/* <link rel="icon" href="/favicon.png" sizes="32x32" type="image/png" /> */}
+        {/* <link rel="apple-touch-icon" href="/apple-touch-icon.png" /> */}
+        <title>Braidery</title>
+        <meta
+          name="description"
+          content="Your Hair Care Companion, Tailored to Every Texture."
+        />
       </head>
       <body
         className={`${Poppins.variable} min-h-screen text-brown-600 dark:text-brown-800`}
@@ -29,7 +40,7 @@ export default function Layout({ children }: { children: ReactNode }) {
           <NavBar />
         </header>
         <Toaster richColors />
-        <main className={`${Poppins.variable}flex-grow`}>{children}</main>
+        <main className={`${Poppins.variable} flex-grow`}>{children}</main>
         <footer className="border-t py-8 text-center bg-peach-300 dark:bg-darkButton text-[#18181B]">
           &copy; {new Date().getFullYear()} BRAIDERY. All rights reserved.
         </footer>
